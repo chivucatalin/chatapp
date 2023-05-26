4ad2e070f9bce670b614aee8eb1cd34695b4dd0c
chivucatalin <80068388+chivucatalin@users.noreply.github.com>
2023-05-26 08:38:25 +0300

    Added notification

--- app/src/main/java/com/chivucatalin/chatapp/firebase/MessagingService.kt
+++ app/src/main/java/com/chivucatalin/chatapp/firebase/MessagingService.kt
@@ -1,10 +1,50 @@
 package com.chivucatalin.chatapp.firebase
 
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.chivucatalin.chatapp.R
+import com.chivucatalin.chatapp.activities.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService : FirebaseMessagingService() {
-    override fun onNewToken(token: String) {
+    val TAG = "FirebaseMessagingService"
+
+    @SuppressLint("LongLogTag")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "Message from: ${remoteMessage.from}")
+
+        if (remoteMessage.notification != null) {
+            showNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+        }
+    }
+
+    private fun showNotification(title: String?, body: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(soundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    /*override fun onNewToken(token: String) {
         super.onNewToken(token)
         //        Log.d("FCM", "Token: " + token);
     }
@@ -12,5 +52,5 @@ class MessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         //        Log.d("FCM", "Message: " + remoteMessage.getNotification().getBody());
-    }
+    }*/
 }
